e56d9161d7c743438deef6ffb7477822990f00c6
Abraar <reinhardt.codes@gmail.com>
2023-08-08 20:29:31 +0600

    update adapter

--- 1. all_dp (Code + Note)/Structural DP/Adapter/adapter.py
+++ 1. all_dp (Code + Note)/Structural DP/Adapter/adapter.py
@@ -1,6 +1,33 @@
 from abc import ABC, abstractmethod
 import json
 
+"""
+Adapter Pattern Example: JSON, XML, and Proto Data Conversion
+
+Scenario:
+This example demonstrates the Adapter pattern used to convert data between three different formats: JSON, XML, and Proto.
+We have separate classes to represent each data format, but we want to provide a uniform interface for data conversion,
+allowing clients to switch between formats seamlessly.
+
+Implementation:
+The Adapter pattern is applied by creating adapter classes that implement a common interface (`Adapter`) for data
+conversion. Each adapter class handles the conversion logic between a specific pair of formats (e.g., JSON to XML,
+XML to JSON, Proto to XML). The `Client` class utilizes the adapter to convert data without knowledge of the underlying
+conversion logic.
+
+Usage:
+1. Create instances of JSON, XML, or Proto data classes.
+2. Instantiate the corresponding adapter classes (JsonToXmlAdapter, XmlToJsonAdapter, ProtoToXmlAdapter).
+3. Initialize the `Client` class with the desired adapter.
+4. Use the `Client` to convert data between different formats using the adapter.
+
+Note:
+In this example, the Proto to XML and XML to JSON conversion logic is the same. In a real-world scenario, different
+conversion logic may exist, and the Adapter pattern would help encapsulate these details, promoting code flexibility
+and maintainability.
+"""
+
+
 
 # Base adapter class implementing the common conversion logic
 class Adapter(ABC):
@@ -30,6 +57,15 @@ class XML:
         return self.data
 
 
+# Proto class representing a Proto object
+class Proto:
+    def __init__(self, data: str = "") -> None:
+        self.data = data
+
+    def __str__(self):
+        return self.data
+
+
 # Adapter class to convert JSON to XML
 class JsonToXmlAdapter(Adapter):
     def convert(self, data: JSON) -> XML:
@@ -49,6 +85,17 @@ class XmlToJsonAdapter(Adapter):
         # Logic to convert XML data to JSON
         json_data = json.dumps({"xml_data": str(data)})
         return JSON(json_data)
+
+
+# Adapter class to convert Proto to XML
+class ProtoToXmlAdapter(Adapter):
+    def convert(self, data: Proto) -> XML:
+        """
+        Converts Proto data to XML format using a specific conversion logic.
+        """
+        # Logic to convert Proto data to XML
+        xml_data = "<xml>{}</xml>".format(data)
+        return XML(xml_data)
 
 
 # Client class utilizing the adapter pattern
@@ -72,11 +119,16 @@ if __name__ == '__main__':
     client1 = Client(json_to_xml_adapter)
     json_data = JSON('{"key": "value"}')
     xml_data = client1.convert(json_data)
-    print("XML Data:", xml_data)
-    
+    print("JSON to XML Data:", xml_data)
+
     xml_to_json_adapter = XmlToJsonAdapter()
     client2 = Client(xml_to_json_adapter)
     xml_data = XML('<xml><key>value</key></xml>')
     json_data = client2.convert(xml_data)
-    print("JSON Data:", json_data)
-    
+    print("XML to JSON Data:", json_data)
+
+    proto_to_xml_adapter = ProtoToXmlAdapter()
+    client3 = Client(proto_to_xml_adapter)
+    proto_data = Proto('proto-data')
+    xml_data = client3.convert(proto_data)
+    print("Proto to XML Data:", xml_data)
